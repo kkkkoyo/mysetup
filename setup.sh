4bcb28b099d6b4b91338cb352f6ac8d53cc40083
kkkkoyo <koyoarai512@gmail.com>
2019-12-22 03:38:18 +0900

    add

--- setup.sh
+++ setup.sh
@@ -5,5 +5,15 @@ brew cask install notion
 brew cask install skitch
 brew cask install slack
 brew cask install visual-studio-code
-brew cask install iterm
+brew cask install iterm2
 brew cask install sourcetree
+brew cask install Postman
+brew cask install mamp
+brew cask install keyboardcleantool
+brew cask install giphy-capture
+brew cask install docker
+brew cask install unity-hub
+# brew cask install apowersoft-heic-converter
+# appstore install command
+brew install mas
+# brew cask install gtaskspro
