d0070804c5d1c44879d647bf3b91829fdb9baa1f
kkkkoyo <koyoarai512@gmail.com>
2021-03-09 14:41:22 +0900

    update

---
+++ Brewfile
@@ -0,0 +1,27 @@
+cask "zoom"
+cask "resolutionator"
+cask "obs"
+cask "alfred"
+cask "dash"
+cask "karabiner-elements"
+cask "notion"
+cask "skitch"
+cask "slack"
+cask "visual-studio-code"
+cask "iterm2"
+cask "sourcetree"
+cask "Postman"
+cask "mamp"
+cask "keyboardcleantool"
+cask "docker"
+cask "unity-hub"
+
+# appstore  command
+# gtaskspo
+# xcode 11.3
+# GIPHY CAPTURE
+#key note
+
+# apowersoft-heic-converter
+# spark ar
+# touch designer
--- setup.sh
+++ setup.sh
@@ -1,26 +1,9 @@
-brew cask install alfred
-brew cask install dash
-brew cask install karabiner-elements
-brew cask install notion
-brew cask install skitch
-brew cask install slack
-brew cask install visual-studio-code
-brew cask install iterm2
-brew cask install sourcetree
-brew cask install Postman
-brew cask install mamp
-brew cask install keyboardcleantool
-brew cask install docker
-brew cask install unity-hub
 
-# appstore install command
-brew install mas
 
-mas install 1054781057 # gtaskspo
-mas install 497799835 # xcode 11.3
-mas install 668208984 # GIPHY CAPTURE
-mas install 409183694 #key note
+# install homebrew if it isn't installed already
+if ! hash brew 2>/dev/null; then
+  echo "Install homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
 
-# apowersoft-heic-converter
-# spark ar
-# touch designer
+brew bundle
